84d9ce1e9cc03eeb9030c06b2aaafc11e4140d65
Your Name <you@example.com>
2020-03-26 19:47:19 -0300

    Mobileimplementado

---
+++ mobile/src/pages/Incidents/styles.js
@@ -0,0 +1,77 @@
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 24,
+        paddingTop: Constants.statusBarHeight +20,
+    },
+    
+    header : {
+        flexDirection: 'row',
+        justifyContent: 'space-between', 
+        alignItems: 'center'
+    }, 
+
+    headerText: {
+        fontSize: 15,
+        color: '#737380'
+    }, 
+
+    headerTextBold: {
+        fontWeight : 'bold'
+    },
+
+    title : {
+        fontSize : 30, 
+        marginBottom: 16, 
+        marginTop : 48, 
+        color : '#13131a', 
+        fontWeight: 'bold'
+    }, 
+
+    description : {
+        fontSize : 16, 
+        lineHeight : 24, 
+        color: '#727280'
+    },
+
+    incidentList : {
+        marginTop : 32
+    },
+
+    incident : {
+        padding: 14,
+        borderRadius : 8, 
+        backgroundColor : '#FFF',
+        marginBottom: 10, 
+    }, 
+
+    incidentProperty: {
+        fontSize: 14, 
+        color: '#41414d',
+        fontWeight : 'bold'
+    }, 
+
+    incidentValue: {
+        marginTop : 4, 
+        fontSize: 15,
+        marginBottom: 14, 
+        color: '#737380'
+    },
+
+    detailsButton : {
+       flexDirection: 'row', 
+       justifyContent: 'space-between', 
+       alignItems: 'center', 
+    }, 
+
+    detailsButtonText : {
+        color : '#e02041', 
+        fontSize : 15, 
+        fontWeight: 'bold'
+    }
+
+})
