19e4f244df7ff41d7ed7a92235c0c36507fe5296
Your Name <you@example.com>
2020-03-27 23:09:46 -0300

    Validação em rotas

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,14 @@
 const express  = require('express')
 const routes = require('./routes')
 const cors = require('cors');
+const { errors } = require('celebrate');
 const port = 3333;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-
+app.use(errors())
 
 app.listen(port, (err) =>{
     if(err){
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
+
+const { celebrate, Segments, Joi} = require('celebrate');
+
 const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/IncidentController')
 const ProfileController = require('./controllers/ProfileController')
 const SessionController = require('./controllers/SessionController')
+
 const routes = express.Router()
 
 
@@ -10,22 +14,57 @@ const routes = express.Router()
 routes.get('/ongs', OngController.index)
 
 ///POST - INSERIR ONG
-routes.post('/ongs', OngController.create)
+routes.post('/ongs', celebrate({
+//VALIDAÇÂO COM CELEBRATE
+[Segments.BODY]: Joi.object().keys({
+    name : Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.string().required().min(9).max(11), 
+    city: Joi.string().required(),
+    uf : Joi.string().required().length(2)
+})
+
+
+}), OngController.create)
 
 /// GET - LISTAR CASOS/INCIDENT
 routes.get('/incidents', IncidentController.index)
 
+
+
 ///POST - INSERIR CASO/INCIDENT
-routes.post('/incidents', IncidentController.create)
+routes.post('/incidents', celebrate({
+
+
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown(), 
+    
+    [Segments.BODY]: Joi.object().keys({
+        title : Joi.string().required().min(5).max(40),
+        description: Joi.string().required().min(15),
+        value: Joi.number().required()
+    })
+
+ }), IncidentController.create)
+
 
 ///DELETE - DELETAR CASO/INCIDENT
 routes.delete('/incidents/:id', IncidentController.delete)
 
 //GET - LISTAR CASOS DE ONG ESPECÌFICA
-routes.get('/profile', ProfileController.index);
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown()
+}), ProfileController.index);
 
 //POST - LOGIN
-routes.post('/sessions', SessionController.create);
+routes.post('/sessions', celebrate({
+    [Segments.BODY]: Joi.object({
+        id: Joi.string().required().min(8).max(8)
+    })
+}), SessionController.create);
 
 
 module.exports = routes;
